ac64126e54f2e74c8e831e78e6cf1429cf9eecd1
Henry Bautista <henry.bautista.prado@icloud.com>
2023-07-17 19:52:11 -0600

    Navbar corregida

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Navbar from './components/navegation/Navbar';
-import Dashboard from "./components/pages/Dashboard";
-import Login from "./components/pages/Login";
-import Register from "./components/pages/Register";  
+import React from 'react'
+import "../src/App.css"
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Navbar from './components/navegation/Navbar' 
+import Dashboard from './components/pages/Dashboard'
+import Login from "./components/pages/Login"
+import Register from "./components/pages/Register"
 
-  function App(){
-    return (
-      <div className="App">
-        <Router>
-          <Navbar />
-        </Router>
-      </div>
-    )
-  }
+function App() {
+  return (
+    <div className='App'>
+      <Router>
+        <Navbar />
+      </Router>
+    </div>
+  )
+}
 
-export default App;
+export default App
--- src/components/navegation/Navbar.jsx
+++ src/components/navegation/Navbar.jsx
@@ -1,31 +1,32 @@
-import Container from 'react-bootstrap/Container';
-import {Link} from "react-router-dom"; 
-import nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import React from 'react'
+import { Link } from 'react-router-dom'
 
-function ColorSchemesExample() {
+const Navbar = () => {
   return (
-    <>
- <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <Link class="nav-link" to="http://localhost:5173/">Home</Link>
+ <div>
+<nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  <div className="collapse navbar-collapse" id="navbarNav">
+    <ul className="navbar-nav">
+    <li className="nav-item">
+        <Link className="nav-link" to="http://localhost:5173/">Home</Link>
       </li>
-      <li class="nav-item">
-        <Link class="nav-link" to="/Login" >Login</Link>
+      <li className="nav-item">
+        <Link className="nav-link" to="/Dashboard">Dashboard</Link>
       </li>
-      <li class="nav-item">
-        <Link class="nav-link" to="/Register">Register</Link>
+      <li className="nav-item">
+        <Link className="nav-link" to="/Register">Registro</Link>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" to="/Dashboard">Dashboard</a>
+      <li className="nav-item">
+        <Link className="nav-link" to="/Login">Login</Link>
       </li>
     </ul>
   </div>
 </nav>
-    </>
-  );
+    </div>
+  )
 }
 
-export default ColorSchemesExample;
+export default Navbar
